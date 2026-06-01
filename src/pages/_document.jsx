ca1ee2f3f5f3074239680a48155ce5d203e786bc
Azaraely <bisho.kolta88@gmail.com>
2026-06-01 17:52:53 +0200

    Build GA4

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -5,6 +5,20 @@ export default function Document() {
     <Html lang="it">
       <Head>
         <meta charSet="utf-8" />
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-TF90EZE3NB"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-TF90EZE3NB');
+            `,
+          }}
+        />
         <meta name="theme-color" content="#00d4ff" />
         <link rel="icon" href="/favicon.png" />
       </Head>
